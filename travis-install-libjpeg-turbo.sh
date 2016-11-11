853f117dac809886ce5cd7313ee934eb0b939db2
Shunsuke Michii <harukasan@me.com>
2016-11-11 13:12:23 +0900

    Remove sudo.

--- travis-install-libjpeg-turbo.sh
+++ travis-install-libjpeg-turbo.sh
@@ -3,6 +3,6 @@
 if [ "$1" != "" ]; then
   cd /tmp
   wget http://downloads.sourceforge.net/project/libjpeg-turbo/$1/libjpeg-turbo-official_$1_amd64.deb
-  sudo dpkg -x libjpeg-turbo-official_$1_amd64.deb /tmp/libjpeg-turbo-official
+  dpkg -x libjpeg-turbo-official_$1_amd64.deb /tmp/libjpeg-turbo-official
   mv /tmp/libjpeg-turbo-official/opt/libjpeg-turbo /tmp/libjpeg-turbo
 fi
