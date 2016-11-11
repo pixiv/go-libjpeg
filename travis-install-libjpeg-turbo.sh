20c3f3c81317a3cb0a442fe556fcb4f7a690efc1
Shunsuke Michii <harukasan@me.com>
2016-11-11 12:47:11 +0900

    Remove dpkg install to run without sudo permission.

--- travis-install-libjpeg-turbo.sh
+++ travis-install-libjpeg-turbo.sh
@@ -3,7 +3,6 @@
 if [ "$1" != "" ]; then
   cd /tmp
   wget http://downloads.sourceforge.net/project/libjpeg-turbo/$1/libjpeg-turbo-official_$1_amd64.deb
-  sudo dpkg -i libjpeg-turbo-official_$1_amd64.deb
-  find /opt/libjpeg-turbo/lib64
-  find /opt/libjpeg-turbo/include
+  sudo dpkg -x libjpeg-turbo-official_$1_amd64.deb /tmp/libjpeg-turbo-official
+  mv /tmp/libjpeg-turbo-official/opt/libjpeg-turbo /tmp/libjpeg-turbo
 fi
