2a84e7e7698af4b57d07fb84702a81839ab76123
Michii Shunsuke <harukasan@pixiv.com>
2019-08-21 15:49:02 +0900

    Add a test for issue #51.

---
+++ jpeg/issue51_test.go
@@ -0,0 +1,19 @@
+package jpeg_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/pixiv/go-libjpeg/jpeg"
+)
+
+var data = []byte("\xff\xd8\xff\xdb\x00C\x000000000000000" +
+	"00000000000000000000" +
+	"00000000000000000000" +
+	"00000000000\xff\xc9\x00\v\b00\x000" +
+	"\x01\x01\x14\x00\xff\xda\x00\b\x01\x010\x00?\x0000")
+
+// https://github.com/pixiv/go-libjpeg/issues/51
+func TestIssue51(t *testing.T) {
+	jpeg.Decode(bytes.NewReader(data), &jpeg.DecoderOptions{})
+}
